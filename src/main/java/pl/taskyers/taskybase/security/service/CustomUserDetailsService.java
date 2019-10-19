52527ecfe98ab8931c928af4fd9e8cdb13fe00cd
Marcin <marcin05031997@gmail.com>
2019-10-19 22:35:05 +0200

    Implement security

---
+++ src/main/java/pl/taskyers/taskybase/security/service/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package pl.taskyers.taskybase.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pl.taskyers.taskybase.core.entity.UserEntity;
+import pl.taskyers.taskybase.core.repository.UserRepository;
+
+import java.util.HashSet;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    
+    private UserRepository userRepository;
+    
+    @Autowired
+    public void setUserRepository(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+    
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity user=userRepository.findByUsername(username).get();
+    
+        if(user==null)
+            throw new UsernameNotFoundException("User not found");
+        org.springframework.security.core.userdetails.User userDetails=
+                new org.springframework.security.core.userdetails.User(user.getUsername(),
+                                                                       user.getPassword(),
+                                                                       new HashSet<SimpleGrantedAuthority>());
+        return userDetails;
+    }
+    
+}
