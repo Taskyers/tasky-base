f40b3f2c118301dd2d81f01d49d3ffc3797ad40c
Marcin <marcin05031997@gmail.com>
2019-10-21 00:24:03 +0200

    Security tests

--- src/test/resources/pl/taskyers/taskybase/integration/db/insert_data.sql
+++ src/test/resources/pl/taskyers/taskybase/integration/db/insert_data.sql
@@ -4,3 +4,7 @@ INSERT INTO verification_tokens(user_id, token)
 VALUES (1, 'tested-token');
 INSERT INTO password_recovery_tokens(user_id, token)
 VALUES (1, 'tested-token');
+INSERT INTO users(username, password, name, surname, email, enabled)
+VALUES ('enabled', '$2a$10$0k1y57DwGGZ8iKY5jpd6fum./qxDxq24lGsi8ChagpXgEHHVV0V6W', 'Tester', 'Good', 'enabled@gmail.com', 1);
+INSERT INTO users(username, password, name, surname, email, enabled)
+VALUES ('notEnabled', '$2a$10$0k1y57DwGGZ8iKY5jpd6fum./qxDxq24lGsi8ChagpXgEHHVV0V6W', 'Tester', 'Bad', 'notEnabled@gmail.com', 0);
