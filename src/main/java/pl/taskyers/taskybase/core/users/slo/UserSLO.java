5add5a0463f20b883a5e38e3b59e62da3dde58c2
jsildatk <jakub.sildatk@gmail.com>
2019-10-30 17:36:31 +0000

    Add javadoc and removed selenium from pom

--- src/main/java/pl/taskyers/taskybase/core/users/slo/UserSLO.java
+++ src/main/java/pl/taskyers/taskybase/core/users/slo/UserSLO.java
@@ -4,16 +4,55 @@ import pl.taskyers.taskybase.core.users.entity.UserEntity;
 
 import java.util.Optional;
 
+/**
+ * Interface for operations on user entity
+ *
+ * @author Jakub Sildatk
+ */
 public interface UserSLO {
     
+    /**
+     * Add new user to database with encoded password
+     *
+     * @param userEntity user entity to be saved
+     * @return saved user entity
+     * @since 0.0.1
+     */
     UserEntity registerUser(UserEntity userEntity);
     
+    /**
+     * Update user's password with saving to database
+     *
+     * @param userEntity user entity which password will be updated
+     * @param password   new password
+     * @since 0.0.1
+     */
     void updatePassword(UserEntity userEntity, String password);
     
+    /**
+     * Enable user account with saving to database
+     *
+     * @param userEntity user entity which will be activated
+     * @since 0.0.1
+     */
     void enableUser(UserEntity userEntity);
     
+    /**
+     * Get user entity by email
+     *
+     * @param email
+     * @return optional user entity
+     * @since 0.0.1
+     */
     Optional<UserEntity> getEntityByEmail(String email);
     
+    /**
+     * Get user entity by username
+     *
+     * @param username
+     * @return optional user entity
+     * @since 0.0.1
+     */
     Optional<UserEntity> getEntityByUsername(String username);
     
 }
