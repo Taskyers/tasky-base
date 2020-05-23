baa3c2dd466b955015f3ee835ca8c2c09fd850f7
jsildatk <jakub.sildatk@gmail.com>
2020-05-23 15:29:09 +0200

    Refactoring

--- src/test/java/pl/taskyers/taskybase/registration/CucumberRunnerRegistration.java
+++ src/test/java/pl/taskyers/taskybase/registration/CucumberRunnerRegistration.java
@@ -5,7 +5,9 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "pretty", "junit:target/cucumber/cucumber_registration.xml",
-        "json:target/cucumber/cucumber_registration.json" }, features = "src/test/resources/pl/taskyers/taskybase/registration/")
+@CucumberOptions(plugin = { "pretty",
+        "junit:target/cucumber/cucumber_registration.xml",
+        "json:target/cucumber/cucumber_registration.json" },
+        features = "src/test/resources/pl/taskyers/taskybase/registration/")
 public class CucumberRunnerRegistration {
 }
